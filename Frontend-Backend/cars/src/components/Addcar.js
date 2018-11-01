d793d1e97d43bb5921ec1eee931b8a9ed88e8f88
Danny Yen <danny.y.yen@gmail.com>
2018-11-01 12:11:54 +0200

    updated carshop with edit button, add car component and save function

---
+++ Frontend-Backend/cars/src/components/Addcar.js
@@ -0,0 +1,121 @@
+import React, { Component } from "react";
+import SkyLight from "react-skylight";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import SaveIcon from "@material-ui/icons/Save";
+
+import TextField from "@material-ui/core/TextField";
+
+class Addcar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "",
+      model: "",
+      color: "",
+      year: "",
+      fuel: "",
+      price: ""
+    };
+    this.addModal = React.createRef();
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  saveCar = () => {
+    const car = {
+      brand: this.state.brand,
+      model: this.state.model,
+      color: this.state.color,
+      year: this.state.year,
+      fuel: this.state.fuel,
+      price: this.state.price
+    };
+    this.props.saveCar(car);
+    this.setState({brand: "", model: "", color: "", year: "", fuel: "", price: ""})
+    this.addModal.current.hide();
+  };
+  render() {
+    const addDialog = {
+      width: "30%",
+      height: "300px",
+      marginLeft: "-15%"
+    };
+
+    return (
+      <div>
+        <Button
+          style={{ margin: 10 }}
+          variant="contained"
+          color="primary"
+          onClick={() => this.addModal.current.show()}
+        >
+          <AddIcon />
+          New Car
+        </Button>
+        <SkyLight
+          dialogStyles={addDialog}
+          hideOnOverlayClicked
+          ref={this.addModal}
+          title="New Car"
+        >
+          <TextField
+            placeholder="Brand"
+            name="brand"
+            onChange={this.handleChange}
+            value={this.state.brand}
+          />
+          <br />
+          <TextField
+            placeholder="Model"
+            name="model"
+            onChange={this.handleChange}
+            value={this.state.model}
+          />
+          <br />
+          <TextField
+            placeholder="Color"
+            name="color"
+            onChange={this.handleChange}
+            value={this.state.color}
+          />
+          <br />
+          <TextField
+            placeholder="Year"
+            name="year"
+            onChange={this.handleChange}
+            value={this.state.year}
+          />
+          <br />
+          <TextField
+            placeholder="Fuel"
+            name="fuel"
+            onChange={this.handleChange}
+            value={this.state.fuel}
+          />
+          <br />
+          <TextField
+            placeholder="Price"
+            name="price"
+            onChange={this.handleChange}
+            value={this.state.price}
+          />
+          <br />
+          <Button
+            style={{ margin: 15 }}
+            onClick={this.saveCar}
+            variant="contained"
+            color="default"
+          >
+            <SaveIcon />
+            SAVE
+          </Button>
+        </SkyLight>
+      </div>
+    );
+  }
+}
+
+export default Addcar;
--- Frontend-Backend/cars/src/components/Carlist.js
+++ Frontend-Backend/cars/src/components/Carlist.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import ReactTable from "react-table";
-import 'react-table/react-table.css'
-import Snackbar from '@material-ui/core/Snackbar';
-import Fade from '@material-ui/core/Fade';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
-import Tooltip from '@material-ui/core/Tooltip';
-
-
-
+import "react-table/react-table.css";
+import Snackbar from "@material-ui/core/Snackbar";
+import Fade from "@material-ui/core/Fade";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Tooltip from "@material-ui/core/Tooltip";
+import Addcar from "./Addcar";
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
 
 class Carlist extends Component {
   constructor(props) {
@@ -16,65 +16,140 @@ class Carlist extends Component {
     this.state = {
       cars: [],
       open: false,
-
+      msg: ""
     };
   }
   componentDidMount() {
-   this.listCars();
+    this.listCars();
   }
- 
+  renderEditable = cellInfo => {
+    return (
+      <div
+        style={{ backgroundColor: "#fafafa" }}
+        contentEditable
+        suppressContentEditableWarning
+        onBlur={e => {
+          const data = [...this.state.cars];
+          data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+          this.setState({ cars: data });
+        }}
+        dangerouslySetInnerHTML={{
+          __html: this.state.cars[cellInfo.index][cellInfo.column.id]
+        }}
+      />
+    );
+  };
+  updateCar = (car, link) => {
+    console.log(link);
+    console.log(car);
+    fetch(link, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(car)
+    })
+      .then(response => {
+        this.listCars();
+        this.setState({
+          showSnack: true,
+          msg: "Car Updated"
+        });
+      })
+      .catch(err => {
+        console.error(err);
+        this.setState({ msg: "Error in updating" });
+      });
+  };
+  saveCar = car => {
+    fetch("https://carstockrest.herokuapp.com/cars", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(car)
+    }).then(response => {
+      this.setState({ showSnack: true });
+      this.listCars();
+    });
+  };
 
   handleClose = () => {
     this.setState({ showSnack: false });
   };
 
   listCars = () => {
-      fetch("https://carstockrest.herokuapp.com/cars")
+    fetch("https://carstockrest.herokuapp.com/cars")
       .then(response => response.json())
       .then(responseData => {
-        this.setState({ cars: responseData._embedded.cars })
-      })
-  }
+        this.setState({ cars: responseData._embedded.cars });
+      });
+  };
 
   // Delete a car
-  deleteCar = (link) => {
-      //alert(link); this will alert the 
-    fetch(link, {method: 'DELETE'})
-    .then(response => {
-        this.listCars();
-        this.setState({showSnack: true})
-    })
-  }
+  deleteCar = link => {
+    //alert(link); this will alert the
+    fetch(link, { method: "DELETE" }).then(response => {
+      this.listCars();
+      this.setState({ showSnack: true, msg: "Car deleted" });
+    });
+  };
 
   render() {
-        const columns = [{
-    Header: 'Brand',
-    accessor: 'brand' // String-based value accessors!
-  }, {
-    Header: 'Model',
-    accessor: 'model'
-  }, {
-    Header: 'Color',
-    accessor: 'color'
-  }, {
-    Header: 'fuel',
-    accessor: 'fuel'
-  }, {
-    Header: 'Year',
-    accessor: 'year'
-  }, {
-    Header: 'Price (£)',
-    accessor: 'price'
-  }, {
-    Header: '',
-    accessor: '_links.self.href',
-    Cell: ({value}) => (<Tooltip title="Delete" placement="right-end"><IconButton aria-label="Delete" onClick={() => this.deleteCar(value)}> <DeleteIcon /> </IconButton></Tooltip>)
-  }]
-    return <div>
-        <ReactTable   defaultPageSize={15} filterable={true}
- data={this.state.cars} columns={columns} />
-    <Snackbar message={'Car deleted'} autoHideDuration={3000} open={this.state.showSnack} onClose={this.handleClose} />
-      </div>;
+    const columns = [
+      { Header: "Brand", accessor: "brand", Cell: this.renderEditable },
+      { Header: "Model", accessor: "model", Cell: this.renderEditable },
+      { Header: "Color", accessor: "color", Cell: this.renderEditable },
+      { Header: "fuel", accessor: "fuel", Cell: this.renderEditable },
+      { Header: "Year", accessor: "year", Cell: this.renderEditable },
+      { Header: "Price (£)", accessor: "price", Cell: this.renderEditable },
+      {
+        Header: "",
+        filterable: false,
+        sortable: false,
+        minWidth: 30,
+        accessor: "_links.self.href",
+        Cell: ({ row, value }) => (
+          <Tooltip title="Update">
+            <Button
+              size="small"
+              color="default"
+              onClick={() => this.updateCar(row, value)}
+            >
+              <SaveIcon />
+            </Button>
+          </Tooltip>
+        )
+      },
+      {
+        Header: "",
+        accessor: "_links.self.href",
+        Cell: ({ value }) => (
+          <Tooltip title="Delete">
+            <IconButton
+              aria-label="Delete"
+              onClick={() => this.deleteCar(value)}
+            >
+              {" "}
+              <DeleteIcon />{" "}
+            </IconButton>
+          </Tooltip>
+        )
+      }
+    ];
+    return (
+      <div>
+        <Addcar saveCar={this.saveCar} />
+        <ReactTable
+          defaultPageSize={15}
+          filterable={true}
+          data={this.state.cars}
+          columns={columns}
+        />
+        <Snackbar
+          message={this.state.msg}
+          autoHideDuration={3000}
+          open={this.state.showSnack}
+          onClose={this.handleClose}
+        />
+      </div>
+    );
   }
 }
 
