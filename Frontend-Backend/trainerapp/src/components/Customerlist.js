3df2e0238f67ebf70fa255bcbefd965a4bd3a21b
Danny Yen <danny.y.yen@gmail.com>
2018-11-09 12:10:03 +0200

    added router, react-table, and moment with fetch api

---
+++ Frontend-Backend/trainerapp/src/components/Customerlist.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import '../App.css';
+
+
+class Customerlist extends Component {
+  constructor(props){
+    super(props);
+    this.getCustomersData=this.getCustomersData.bind(this);
+   this.state = { customers: [] };
+
+  }
+  componentDidMount() {
+    this.getCustomersData();
+  }
+//getting customer list from rest api
+  getCustomersData = () => {
+    fetch('https://customerrest.herokuapp.com/api/customers')
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({
+        customers: responseData.content,
+      });
+    })
+  }
+//rendering with React Table
+  render() {
+    return (
+      <div className="App-body">
+        <header className="List-header">
+          <h5 className="List-title">Customer's List</h5>
+        </header>
+        <ReactTable
+        defaultPageSize={10}
+        pageSizeOptions={[5,10,15,20]}
+        data={this.state.customers}
+        columns={[
+            {
+              columns: [
+                {
+                  accessor: "_links.self.href",
+                  show: false
+                },
+                {
+                  Header: "First name",
+                  accessor: "firstname",
+                },
+                {
+                  Header: "Last name",
+                  accessor: "lastname",
+                },
+                {
+                  Header: "Email",
+                  accessor: "email",
+                },
+                {
+                  Header: "Phone",
+                  accessor: "phone",
+                },
+                {
+                  Header: "Address",
+                  accessor: "streetaddress",
+                },
+                {
+                  Header: "Postcode",
+                  accessor: "postcode",
+                },
+                {
+                  Header: "City",
+                  accessor: "city",
+                }
+              ]
+            }
+          ]}
+          filterable={true}
+          className="-highlight" >
+        </ReactTable>
+      </div>
+    );
+  }
+}
+export default Customerlist;
